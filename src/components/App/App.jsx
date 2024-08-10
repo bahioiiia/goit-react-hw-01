d6d43049f8aeb845e9c25305b0705d01d182757a
Іван <65630800+bahioiiia@users.noreply.github.com>
2024-08-10 15:14:09 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,10 +5,8 @@ import officers from "../../officers.json";
 export default function App() {
   return (
     <div>
-      <React.StrictMode>
       <Header />
         <PilotList pilots={officers} />
-      </React.StrictMode>
     </div>
   );
 }
